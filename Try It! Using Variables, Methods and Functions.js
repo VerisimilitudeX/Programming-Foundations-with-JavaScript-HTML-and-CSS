4d7481c8bc7424c8f0f7a9fde13e41c1b5179084
Piyush Acharya <acharyapiyush1@outlook.com>
2022-07-25 15:36:55 -0700

    Update and rename Try It! Using Variables, Methods and Functions.js to Using Variables, Methods and Functions.js

--- Try It! Using Variables, Methods and Functions.js
+++
@@ -1,2 +0,0 @@
-var photo = new SimpleImage("chapel.png");
-print(photo.getWidth())
---
+++ Using Variables, Methods and Functions.js
@@ -0,0 +1,10 @@
+var photo = new SimpleImage("chapel.png");
+print(photo.getWidth())
+print(photo.getRed(53, 79))
+
+function squarenum(num) {
+    num = num ** 2
+    return num
+}
+
+print(squarenum(5))
